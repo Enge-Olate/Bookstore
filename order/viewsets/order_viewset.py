4b6815ef0acf607370483c1cc21ccee7b7b21532
Enge-Olate <marcioalexisolate@live.com>
2026-09-21 11:11:16 -0300

    Viewset inicial.

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewset
 
-from .models import Order
-from .serializers import OrderSerializer
+
+from ..models import Order
+from ..serializers import OrderSerializer
 
 class OrderViewSet(ModelViewset):
     serializer_class = OrderSerializer
